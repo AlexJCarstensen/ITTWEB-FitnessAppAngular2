94c44820cf5424d7df03687453078e627dba49cc
AlexCarstensen <alex.jul.carstensen@gmail.com>
2016-11-23 15:33:57 +0100

    change id in mock and included bootstrap

--- src/app/classes/workoutMock.ts
+++ src/app/classes/workoutMock.ts
@@ -4,14 +4,14 @@ import { Exercise } from './exercise';
 var userMock = "pede_ring@hotmail.com";
 
 var EXERCISES: Exercise[] = [
-    {id: 1, name: "Squats", description: "Bend legs with bar", sets: 3, repetitions: 15},
-    {id: 2, name: "Burpies", description: "Fly down with legs crossed", sets: 4, repetitions: 20},
-    {id: 3, name: "Crunches", description: "Do a barrel roll", sets: 3, repetitions: 15}
+    {_id: 1, name: "Squats", description: "Bend legs with bar", sets: 3, repetitions: 15},
+    {_id: 2, name: "Burpies", description: "Fly down with legs crossed", sets: 4, repetitions: 20},
+    {_id: 3, name: "Crunches", description: "Do a barrel roll", sets: 3, repetitions: 15}
 ]
 
 export const WORKOUTS: Workout[] = [
-    {id: 1, name: "Legs", user: userMock, exercises: EXERCISES},
-    {id: 2, name: "Body", user: userMock, exercises: EXERCISES},
-    {id: 3, name: "Arms", user: userMock, exercises: EXERCISES},
-    {id: 2, name: "Head", user: userMock, exercises: EXERCISES}
+    {_id: 1, name: "Legs", user: userMock, exercises: EXERCISES},
+    {_id: 2, name: "Body", user: userMock, exercises: EXERCISES},
+    {_id: 3, name: "Arms", user: userMock, exercises: EXERCISES},
+    {_id: 2, name: "Head", user: userMock, exercises: EXERCISES}
 ];
